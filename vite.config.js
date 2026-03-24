85e0bac45526116d25f0a2150b4ccfcea494ec7e
Rene Dellefont <renedellefont@gmail.com>
2026-03-24 18:42:44 -0400

    upgrade vite

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,15 @@ import netlify from '@netlify/vite-plugin-tanstack-start'
 import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   server: {
     port: 3000,
   },
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
-    tsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
     tailwindcss(),
     tanstackStart(),
     netlify(),
